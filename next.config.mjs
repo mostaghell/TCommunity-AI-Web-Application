d039a52370f665520dc73884fbdc98a25647f8d0
Tehran Community <paradoxoxkox@gmail.com>
2025-09-06 08:47:04 -0700

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  allowedDevOrigins: ["localhost:3000","192.168.157.1:3000","0.0.0.0:3000"],
+}
+
+export default nextConfig
